7e183c53aa2e893338a1bdf3f53bb775d509bac5
Kasianenko Oleksandr <sanekkas@gmail.com>
2022-04-14 00:12:56 +0300

    done task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const ref = {
+  form: document.querySelector('.login-form'),
+};
 
+ref.form.addEventListener('submit', onSubmitSaveData);
+
+function onSubmitSaveData(event) {
+  event.preventDefault();
+
+  const { email, password } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    alert('Please fill in all the fields!');
+  } else {
+    const formData = new FormData(event.currentTarget);
+    event.currentTarget.reset();
+
+    formData.forEach((value, contentName) => {
+      console.log(`Item: ${contentName}`);
+      console.log(`Value:  ${value}`);
+    });
+  }
+}

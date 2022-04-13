57adc5f2fb0892fd5411dce845b6568da7fca1a4
Kasianenko Oleksandr <sanekkas@gmail.com>
2022-04-14 00:30:44 +0300

    done task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+const ref = {
+  colorTitle: document.querySelector('.color'),
+  changeBtn: document.querySelector('.change-color'),
+  body: document.querySelector('body'),
+};
+
+ref.changeBtn.addEventListener('click', onClickColorChange);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onClickColorChange() {
+  ref.colorTitle.textContent = getRandomHexColor();
+  ref.body.style.backgroundColor = ref.colorTitle.textContent;
 }

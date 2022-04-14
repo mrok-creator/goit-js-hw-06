d57fdce7b66223ad3b4982f3ebf7c3c2bd4f8401
Kasianenko Oleksandr <sanekkas@gmail.com>
2022-04-14 10:56:04 +0300

    done task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,28 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const ref = {
+  input: document.querySelector('input[type="number"]'),
+  crateBtn: document.querySelector('button[data-create]'),
+  btnDestroy: document.querySelector('button[data-destroy]'),
+  builderField: document.querySelector('#boxes'),
+};
+
+ref.crateBtn.addEventListener('click', onClickDivCreate);
+ref.btnDestroy.addEventListener('click', onClickClearAll);
+
+function onClickDivCreate(event) {
+  let starterParam = 30;
+  let resMarkup = '';
+
+  for (let i = 1; i <= ref.input.value; i += 1) {
+    resMarkup += `<div style="width: ${starterParam}px; height: ${starterParam}px; background-color: ${getRandomHexColor()}">
+    </div>`;
+    starterParam += 10;
+  }
+  ref.builderField.insertAdjacentHTML('afterbegin', resMarkup);
+}
+function onClickClearAll(event) {
+  ref.builderField.innerHTML = '';
+}
